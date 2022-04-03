5883a74ab5bf9adee065bd11cbe02af6002959c9
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-04-03 10:42:34 -0400

    Add PagerServer example

---
+++ examples/PagerServer/PagerServer.ino
@@ -0,0 +1,185 @@
+/****************************************************************************************************************************
+  PagerServer.ino
+
+  Ethernet_Generic is a library for the W5x00 Ethernet shields trying to merge the good features of
+  previous Ethernet libraries
+
+  Built by Khoi Hoang https://github.com/khoih-prog/Ethernet_Generic
+ *****************************************************************************************************************************/
+
+/*
+  Pager Server
+
+  A simple server that echoes any incoming messages to all  connected clients. Connect two or more telnet sessions
+  to see how server.available() and server.print() works.
+
+  Created in September 2020 for the Ethernet library  by Juraj Andrassy https://github.com/jandrassy
+
+*/
+
+#include "defines.h"
+
+EthernetServer server(2323);
+
+void setup() 
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.print("\nStarting PagerServer on "); Serial.print(BOARD_NAME);
+  Serial.print(F(" with ")); Serial.println(SHIELD_TYPE); 
+  Serial.println(ETHERNET_GENERIC_VERSION);
+  
+  ETG_LOGWARN(F("Default SPI pinout:"));
+  ETG_LOGWARN1(F("MOSI:"), MOSI);
+  ETG_LOGWARN1(F("MISO:"), MISO);
+  ETG_LOGWARN1(F("SCK:"),  SCK);
+  ETG_LOGWARN1(F("SS:"),   SS);
+  ETG_LOGWARN(F("========================="));
+
+#if defined(ESP8266)
+  // For ESP8266, change for other boards if necessary
+  #ifndef USE_THIS_SS_PIN
+    #define USE_THIS_SS_PIN   D2    // For ESP8266
+  #endif
+
+  ETG_LOGWARN1(F("ESP8266 setCsPin:"), USE_THIS_SS_PIN);
+
+  // For ESP8266
+  // Pin                D0(GPIO16)    D1(GPIO5)    D2(GPIO4)    D3(GPIO0)    D4(GPIO2)    D8
+  // Ethernet_Generic   X                 X            X            X            X        0
+  // D2 is safe to used for Ethernet_Generic libs
+  //Ethernet.setCsPin (USE_THIS_SS_PIN);
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#elif defined(ESP32)
+
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  //Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+  #ifndef USE_THIS_SS_PIN
+    #define USE_THIS_SS_PIN   5   //22    // For ESP32
+  #endif
+
+  ETG_LOGWARN1(F("ESP32 setCsPin:"), USE_THIS_SS_PIN);
+
+  // Must use library patch for Ethernet, EthernetLarge libraries
+  // ESP32 => GPIO2,4,5,13,15,21,22 OK with Ethernet, Ethernet2, EthernetLarge
+  // ESP32 => GPIO2,4,5,15,21,22 OK with Ethernet3
+
+  //Ethernet.setCsPin (USE_THIS_SS_PIN);
+  Ethernet.init (USE_THIS_SS_PIN);
+  
+#elif ETHERNET_USE_RPIPICO
+
+  pinMode(USE_THIS_SS_PIN, OUTPUT);
+  digitalWrite(USE_THIS_SS_PIN, HIGH);
+  
+  // ETHERNET_USE_RPIPICO, use default SS = 5 or 17
+  #ifndef USE_THIS_SS_PIN
+    #if defined(ARDUINO_ARCH_MBED)
+      #define USE_THIS_SS_PIN   17     // For Arduino Mbed core
+    #else  
+      #define USE_THIS_SS_PIN   17    // For E.Philhower core
+    #endif
+  #endif
+
+  ETG_LOGWARN1(F("RPIPICO setCsPin:"), USE_THIS_SS_PIN);
+
+  // Must use library patch for Ethernet, EthernetLarge libraries
+  // For RPI Pico using Arduino Mbed RP2040 core
+  // SCK: GPIO2,  MOSI: GPIO3, MISO: GPIO4, SS/CS: GPIO5
+  // For RPI Pico using E. Philhower RP2040 core
+  // SCK: GPIO18,  MOSI: GPIO19, MISO: GPIO16, SS/CS: GPIO17
+  // Default pin 5/17 to SS/CS
+
+  //Ethernet.setCsPin (USE_THIS_SS_PIN);
+  Ethernet.init (USE_THIS_SS_PIN);
+  
+#else   //defined(ESP8266)
+  // unknown board, do nothing, use default SS = 10
+  #ifndef USE_THIS_SS_PIN
+    #define USE_THIS_SS_PIN   10    // For other boards
+  #endif
+
+  #if defined(BOARD_NAME)
+    ETG_LOGWARN3(F("Board :"), BOARD_NAME, F(", setCsPin:"), USE_THIS_SS_PIN);
+  #else
+    ETG_LOGWARN1(F("Unknown board setCsPin:"), USE_THIS_SS_PIN);
+  #endif
+
+  // For other boards, to change if necessary 
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#endif    // defined(ESP8266)
+
+  // start the ethernet connection and the server:
+  // Use DHCP dynamic IP and random mac
+  uint16_t index = millis() % NUMBER_OF_MAC;
+  // Use Static IP
+  //Ethernet.begin(mac[index], ip);
+  Ethernet.begin(mac[index]);
+
+  //SPIClass SPI2(HSPI);
+  //Ethernet.begin(mac[index], &SPI2);
+
+  // Just info to know how to connect correctly
+  // To change for other SPI
+  Serial.println("=========================");
+  Serial.println("Currently Used SPI pinout:");
+  Serial.print("MOSI:");
+  Serial.println(MOSI);
+  Serial.print("MISO:");
+  Serial.println(MISO);
+  Serial.print("SCK:");
+  Serial.println(SCK);
+  Serial.print("SS:");
+  Serial.println(SS);
+  Serial.println(F("========================="));
+
+  Serial.print(F("Using mac index = "));
+  Serial.println(index);
+
+  Serial.print(F("Connected! IP address: "));
+  Serial.println(Ethernet.localIP());
+
+  if (Ethernet.getChip() == w5500)
+  {
+    Serial.print(F("Speed: "));    Serial.print(Ethernet.speedReport());
+    Serial.print(F(", Duplex: ")); Serial.print(Ethernet.duplexReport());
+    Serial.print(F(", Link status: ")); Serial.println(Ethernet.linkReport());
+  }
+
+  server.begin();
+
+  IPAddress ip = Ethernet.localIP();
+  Serial.println();
+  Serial.print("To access the server, connect with Telnet client to ");
+  Serial.print(ip);
+  Serial.println(" 2323");
+}
+
+void loop() 
+{
+  EthernetClient client = server.available(); // returns first client which has data to read or a 'false' client
+  
+  if (client) 
+  { 
+    // client is true only if it is connected and has data to read
+    String s = client.readStringUntil('\n'); // read the message incoming from one of the clients
+    s.trim(); // trim eventual \r
+    
+    Serial.println(s); // print the message to Serial Monitor
+    client.print("echo: "); // this is only for the sending client
+    server.println(s); // send the message to all connected clients
+    
+#ifndef ARDUINO_ARCH_SAM
+    server.flush(); // flush the buffers
+#endif /* !defined(ARDUINO_ARCH_SAM) */
+  }
+}
