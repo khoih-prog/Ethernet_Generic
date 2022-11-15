6ac07f7cfc277a9f01aed062bb7e0aa30e828315
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-11-15 01:49:25 -0500

    v2.7.0 to permit sending data more than 2/4/8/16K
    ### Releases v2.7.0
    
    1. Fix severe limitation to permit sending larger data than 2/4/8/16K buffer
    2. Add examples [EthernetWebServer_BigData](https://github.com/khoih-prog/Ethernet_Generic/tree/main/examples/EthernetWebServer_BigData) to demo how to send larger data than 2/4/8/16K buffer
    3. Optimize code
    4. Clean up
    5. Update `Packages' Patches`

---
+++ examples/EthernetWebServer_BigData/EthernetWebServer_BigData.ino
@@ -0,0 +1,353 @@
+/****************************************************************************************************************************
+  EthernetWebServer_BigData.ino
+
+  EthernetWebServer is a library for the Ethernet shields to run WebServer
+
+  Based on and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+  Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer
+  Licensed under MIT license
+ *****************************************************************************************************************************/
+
+#include "defines.h"
+
+EthernetWebServer server(80);
+
+// Adjust according to your board's heap size. Too large => crash
+#if (ESP32 || ESP8266) || (ETHERNET_USE_NRF528XX) || (ETHERNET_USE_RPIPICO)
+  #define MULTIPLY_FACTOR       3.0f
+  
+#else
+  #define MULTIPLY_FACTOR       1.0f
+#endif
+
+// In bytes
+#define STRING_SIZE         (8192 * MULTIPLY_FACTOR)
+
+#define BUFFER_SIZE         512
+char temp[BUFFER_SIZE];
+
+void createPage(String &pageInput)
+{
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+  int day = hr / 24;
+
+  snprintf(temp, BUFFER_SIZE - 1,
+           "<html>\
+<head>\
+<meta http-equiv='refresh' content='5'/>\
+<title>EthernetWebServer_BigData-%s</title>\
+<style>\
+body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+</style>\
+</head>\
+<body>\
+<h2>EthernetWebServer_W5500!</h2>\
+<h3>running on %s</h3>\
+<p>Uptime: %d d %02d:%02d:%02d</p>\
+</body>\
+</html>", BOARD_NAME, BOARD_NAME, day, hr % 24, min % 60, sec % 60);
+
+  pageInput = temp;
+}
+
+String out;
+
+void handleRoot()
+{
+  //out.reserve(STRING_SIZE);
+
+  // clear the String to start over
+  out = String();
+
+  createPage(out);
+
+  out += "<html><body>\r\n<table><tr><th>INDEX</th><th>DATA</th></tr>";
+
+  for (uint16_t lineIndex = 0; lineIndex < (100 * MULTIPLY_FACTOR); lineIndex++)
+  {
+    out += "<tr><td>";
+    out += String(lineIndex);
+    out += "</td><td>";
+    out += "WiFiWebServer_BigData_ABCDEFGHIJKLMNOPQRSTUVWXYZ</td></tr>";
+  }
+
+  out += "</table></body></html>\r\n";
+
+  Serial.print(F("String Len = ")); Serial.println(out.length());
+
+  server.send(200, F("text/html"), out);
+}
+
+void handleNotFound()
+{
+  String message = F("File Not Found\n\n");
+
+  message += F("URI: ");
+  message += server.uri();
+  message += F("\nMethod: ");
+  message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
+  message += F("\nArguments: ");
+  message += server.args();
+  message += F("\n");
+
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, F("text/plain"), message);
+}
+
+void initEthernet()
+{
+#if USE_ETHERNET_PORTENTA_H7
+  ET_LOGWARN(F("======== USE_PORTENTA_H7_ETHERNET ========"));
+#elif USE_NATIVE_ETHERNET
+  ET_LOGWARN(F("======== USE_NATIVE_ETHERNET ========"));
+#elif USE_ETHERNET_GENERIC
+  ET_LOGWARN(F("=========== USE_ETHERNET_GENERIC ==========="));
+#elif USE_ETHERNET_ESP8266
+  ET_LOGWARN(F("=========== USE_ETHERNET_ESP8266 ==========="));
+#elif USE_ETHERNET_ENC
+  ET_LOGWARN(F("=========== USE_ETHERNET_ENC ==========="));
+#else
+  ET_LOGWARN(F("========================="));
+#endif
+
+#if !(USE_NATIVE_ETHERNET || USE_ETHERNET_PORTENTA_H7)
+
+#if (USING_SPI2)
+#if defined(CUR_PIN_MISO)
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), CUR_PIN_MOSI);
+  ET_LOGWARN1(F("MISO:"), CUR_PIN_MISO);
+  ET_LOGWARN1(F("SCK:"),  CUR_PIN_SCK);
+  ET_LOGWARN1(F("SS:"),   CUR_PIN_SS);
+  ET_LOGWARN(F("========================="));
+#endif
+#else
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), MOSI);
+  ET_LOGWARN1(F("MISO:"), MISO);
+  ET_LOGWARN1(F("SCK:"),  SCK);
+  ET_LOGWARN1(F("SS:"),   SS);
+  ET_LOGWARN(F("========================="));
+#endif
+
+#if defined(ESP8266)
+  // For ESP8266, change for other boards if necessary
+#ifndef USE_THIS_SS_PIN
+#define USE_THIS_SS_PIN   D2    // For ESP8266
+#endif
+
+  ET_LOGWARN1(F("ESP8266 setCsPin:"), USE_THIS_SS_PIN);
+
+#if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+  // For ESP8266
+  // Pin                D0(GPIO16)    D1(GPIO5)    D2(GPIO4)    D3(GPIO0)    D4(GPIO2)    D8
+  // EthernetGeneric    X                 X            X            X            X        0
+  // Ethernet_ESP8266   0                 0            0            0            0        0
+  // D2 is safe to used for Ethernet, Ethernet2, Ethernet3, EthernetLarge libs
+  // Must use library patch for Ethernet, EthernetLarge libraries
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#elif USE_CUSTOM_ETHERNET
+
+  // You have to add initialization for your Custom Ethernet here
+  // This is just an example to setCSPin to USE_THIS_SS_PIN, and can be not correct and enough
+  Ethernet.init(USE_THIS_SS_PIN);
+
+#endif  //( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+
+#elif defined(ESP32)
+
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  //Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+#ifndef USE_THIS_SS_PIN
+#define USE_THIS_SS_PIN   5   //22    // For ESP32
+#endif
+
+  ET_LOGWARN1(F("ESP32 setCsPin:"), USE_THIS_SS_PIN);
+
+  // For other boards, to change if necessary
+#if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+  // Must use library patch for Ethernet, EthernetLarge libraries
+  // ESP32 => GPIO2,4,5,13,15,21,22 OK with Ethernet, Ethernet2, EthernetLarge
+  // ESP32 => GPIO2,4,5,15,21,22 OK with Ethernet3
+
+  //Ethernet.setCsPin (USE_THIS_SS_PIN);
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#elif USE_CUSTOM_ETHERNET
+
+  // You have to add initialization for your Custom Ethernet here
+  // This is just an example to setCSPin to USE_THIS_SS_PIN, and can be not correct and enough
+  Ethernet.init(USE_THIS_SS_PIN);
+
+#endif  //( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+
+#elif ETHERNET_USE_RPIPICO
+
+  pinMode(USE_THIS_SS_PIN, OUTPUT);
+  digitalWrite(USE_THIS_SS_PIN, HIGH);
+
+  // ETHERNET_USE_RPIPICO, use default SS = 5 or 17
+#ifndef USE_THIS_SS_PIN
+#if defined(ARDUINO_ARCH_MBED)
+#define USE_THIS_SS_PIN   5     // For Arduino Mbed core
+#else
+#define USE_THIS_SS_PIN   17    // For E.Philhower core
+#endif
+#endif
+
+  ET_LOGWARN1(F("RPIPICO setCsPin:"), USE_THIS_SS_PIN);
+
+  // For other boards, to change if necessary
+#if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+  // Must use library patch for Ethernet, EthernetLarge libraries
+  // For RPI Pico using Arduino Mbed RP2040 core
+  // SCK: GPIO2,  MOSI: GPIO3, MISO: GPIO4, SS/CS: GPIO5
+  // For RPI Pico using E. Philhower RP2040 core
+  // SCK: GPIO18,  MOSI: GPIO19, MISO: GPIO16, SS/CS: GPIO17
+  // Default pin 5/17 to SS/CS
+
+  //Ethernet.setCsPin (USE_THIS_SS_PIN);
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#endif    //( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+
+#else   //defined(ESP8266)
+  // unknown board, do nothing, use default SS = 10
+#ifndef USE_THIS_SS_PIN
+#define USE_THIS_SS_PIN   10    // For other boards
+#endif
+
+#if defined(BOARD_NAME)
+  ET_LOGWARN3(F("Board :"), BOARD_NAME, F(", setCsPin:"), USE_THIS_SS_PIN);
+#else
+  ET_LOGWARN1(F("Unknown board setCsPin:"), USE_THIS_SS_PIN);
+#endif
+
+  // For other boards, to change if necessary
+#if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC || USE_NATIVE_ETHERNET )
+  // Must use library patch for Ethernet, Ethernet2, EthernetLarge libraries
+
+  Ethernet.init (USE_THIS_SS_PIN);
+
+#elif USE_CUSTOM_ETHERNET
+
+  // You have to add initialization for your Custom Ethernet here
+  // This is just an example to setCSPin to USE_THIS_SS_PIN, and can be not correct and enough
+  Ethernet.init(USE_THIS_SS_PIN);
+
+#endif  //( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+
+#endif    // defined(ESP8266)
+
+#endif    // #if !(USE_NATIVE_ETHERNET)
+
+  // start the ethernet connection and the server:
+  // Use DHCP dynamic IP and random mac
+  uint16_t index = millis() % NUMBER_OF_MAC;
+  // Use Static IP
+  //Ethernet.begin(mac[0], ip);
+  Ethernet.begin(mac[index]);
+
+#if !(USE_NATIVE_ETHERNET || USE_ETHERNET_PORTENTA_H7)
+  ET_LOGWARN(F("========================="));
+
+#if ( defined( ESP32 )  && USE_ETHERNET_GENERIC )
+  // Just info to know how to connect correctly
+  // To change for other SPI
+  ET_LOGWARN(F("Currently Used SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), PIN_MOSI);
+  ET_LOGWARN1(F("MISO:"), PIN_MISO);
+  ET_LOGWARN1(F("SCK:"),  PIN_SCK);
+  ET_LOGWARN1(F("SS:"),   PIN_SS);
+#else
+#if defined(CUR_PIN_MISO)
+  ET_LOGWARN(F("Currently Used SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), CUR_PIN_MOSI);
+  ET_LOGWARN1(F("MISO:"), CUR_PIN_MISO);
+  ET_LOGWARN1(F("SCK:"),  CUR_PIN_SCK);
+  ET_LOGWARN1(F("SS:"),   CUR_PIN_SS);
+#else
+  ET_LOGWARN(F("Currently Used SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), MOSI);
+  ET_LOGWARN1(F("MISO:"), MISO);
+  ET_LOGWARN1(F("SCK:"),  SCK);
+  ET_LOGWARN1(F("SS:"),   SS);
+#endif
+#endif
+
+  ET_LOGWARN(F("========================="));
+
+#elif (USE_ETHERNET_PORTENTA_H7)
+
+  if (Ethernet.hardwareStatus() == EthernetNoHardware)
+  {
+    SerialDebug.println("No Ethernet found. Stay here forever");
+
+    while (true)
+    {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+
+  if (Ethernet.linkStatus() == LinkOFF)
+  {
+    SerialDebug.println("Not connected Ethernet cable");
+  }
+
+#endif
+
+  SerialDebug.print(F("Using mac index = "));
+  SerialDebug.println(index);
+
+  SerialDebug.print(F("Connected! IP address: "));
+  SerialDebug.println(Ethernet.localIP());
+}
+
+void setup()
+{
+  out.reserve(STRING_SIZE);
+
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+  while (!Serial && millis() < 5000);
+
+  delay(200);
+
+  Serial.print(F("\nStart EthernetWebServer_BigData on "));
+  SerialDebug.print(BOARD_NAME);
+  SerialDebug.print(F(" with "));
+  SerialDebug.println(SHIELD_TYPE);
+  SerialDebug.println(ETHERNET_WEBSERVER_VERSION);
+
+  initEthernet();
+
+  server.on(F("/"), handleRoot);
+
+  server.on(F("/inline"), []()
+  {
+    server.send(200, F("text/plain"), F("This works as well"));
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+}
